f72b27644bd55e5df590411f64489cbdc9d5bf02
zoony <58914699+zoony1337@users.noreply.github.com>
2020-03-28 15:17:07 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,43 @@
+### Does not have to be a for loop, could be any process. This was an example. ###
+
+import os
+from colorama import init, Fore
+clear = lambda: os.system('cls') # Change to 'clear' if not on Windows.
+init()
+
+do_stuff = 0
+amount = int(input('Amount: '))
+
+for x in range(amount):
+  done = (((x + 1) / amount))
+  do_stuff += 1 # Process will be quick.
+  if done == 0.1:
+    clear()
+    print(f'{Fore.GREEN}■{Fore.WHITE}■■■■■■■■■ ' + str(int(done * 100)) + '%')
+  elif done == 0.2:
+    clear()
+    print(f'{Fore.GREEN}■■{Fore.WHITE}■■■■■■■■ ' + str(int(done * 100)) + '%')
+  elif done == 0.3:
+    clear()
+    print(f'{Fore.GREEN}■■■{Fore.WHITE}■■■■■■■ ' + str(int(done * 100)) + '%')
+  elif done == 0.4:
+    clear()
+    print(f'{Fore.GREEN}■■■■{Fore.WHITE}■■■■■■ ' + str(int(done * 100)) + '%')
+  elif done == 0.5:
+    clear()
+    print(f'{Fore.GREEN}■■■■■{Fore.WHITE}■■■■■ ' + str(int(done * 100)) + '%')
+  elif done == 0.6:
+    clear()
+    print(f'{Fore.GREEN}■■■■■■{Fore.WHITE}■■■■ ' + str(int(done * 100)) + '%')
+  elif done == 0.7:
+    clear()
+    print(f'{Fore.GREEN}■■■■■■■{Fore.WHITE}■■■ ' + str(int(done * 100)) + '%')
+  elif done == 0.8:
+    clear()
+    print(f'{Fore.GREEN}■■■■■■■■{Fore.WHITE}■■ ' + str(int(done * 100)) + '%')
+  elif done == 0.9:
+    clear()
+    print(f'{Fore.GREEN}■■■■■■■■■{Fore.WHITE}■ ' + str(int(done * 100)) + '%')
+  elif done == 1.0:
+    clear()
+    print(f'{Fore.GREEN}■■■■■■■■■■ ' + str(int(done * 100)) + '%')
